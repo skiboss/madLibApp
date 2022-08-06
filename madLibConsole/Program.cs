0df6df9add8ba2c00f2053700fe97e67e8806497
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-08-06 12:06:12 +0100

    Add project files.

---
+++ madLibConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace madLibConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter Colour: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter Plural Noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter Celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Roses are " + color);
+
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+            Console.WriteLine(" ");
+
+            Console.ReadLine();
+        }
+    }
+}
